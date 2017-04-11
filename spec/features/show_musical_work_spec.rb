985b7c164703ab4a3d3365fa72ceb1a89779b533
Adam Wead <amsterdamos@gmail.com>
2017-04-11 16:00:15 -0400

    Feature test for showing a musical work with complete metadata

---
+++ spec/features/show_musical_work_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+RSpec.feature 'Display a musical work' do
+  let(:work) do
+    MusicalWork.new.tap do |work|
+      work.composer = ["Joe Schmo"]
+      work.title = ["Song"]
+      work.genre =  ["Pop"]
+      work.composition_date = ["today"]
+      work.apply_depositor_metadata("user")
+      work.visibility = "open"
+      work.save
+    end
+  end
+
+  context "with a public work" do
+    it "displays all the fields" do
+      visit(hyrax_musical_work_path(work.id))
+      expect(page).to have_content("Song")
+      expect(page).to have_content("Joe Schmo")
+      expect(page).to have_content("Pop")
+      expect(page).to have_content("today")
+    end
+  end
+end
