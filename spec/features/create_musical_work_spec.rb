4d8d635f8a72a5c5ccf7cff56a80262cdbd68a34
Adam Wead <amsterdamos@gmail.com>
2017-04-12 16:10:36 -0400

    Feature test for creating a new musical work

--- spec/features/create_musical_work_spec.rb
+++ spec/features/create_musical_work_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 include Warden::Test::Helpers
 
-RSpec.feature 'Create a MusicalWork', js: true do
+RSpec.feature 'Create a musical work' do
   context 'a logged in user' do
     let(:user_attributes) do
       { email: 'test@example.com' }
@@ -16,12 +16,16 @@ RSpec.feature 'Create a MusicalWork', js: true do
       login_as user
     end
 
-    xit do
-      visit '/dashboard'
-      click_link "Works"
-      click_link "Add new work"
-      choose "payload_concern", option: "MusicalWork"
-      click_button "Create work"
+    scenario do
+      visit(new_hyrax_musical_work_path)
+      fill_in 'Title', with: 'Mass in B-minor'
+      fill_in 'Composer', with: 'J.S. Bach'
+      fill_in 'Composition date', with: '1735'
+      select 'Classical', from: 'Genre'
+
+      # TODO:
+      # click_button 'Create Work'
+      # expect(page).to have_content 'Test Work'
     end
   end
 end
