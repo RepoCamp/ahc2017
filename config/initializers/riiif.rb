c5a17e2c5b3a812512b6e005930ab08f66a794e8
Justin Coyne <jcoyne@justincoyne.com>
2017-05-05 14:30:36 -0500

    Add authorization for images

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -24,8 +24,7 @@ end
 # Riiif::Image.file_resolver.basic_auth_credentials = [ActiveFedora.fedora.user, ActiveFedora.fedora.password]
 
 
-#Riiif::Image.authorization_service = IIIFAuthorizationService
+Riiif::Image.authorization_service = IIIFAuthorizationService
 
 Riiif.not_found_image = 'app/assets/images/us_404.svg'
 Riiif::Engine.config.cache_duration_in_days = 365
-
