2851034c72998d1e951a74933627aa8541853d68
Tom Johnson <tom@dp.la>
2017-05-07 14:58:12 -0700

    Add cover feature spec

--- spec/features/search_musical_work_spec.rb
+++ spec/features/search_musical_work_spec.rb
@@ -31,4 +31,34 @@ describe "Searching for works" do
       expect(page).to have_content(work.composition_date.first)
     end
   end
+
+  context 'by cover subject' do
+    let(:cover) { ['Magic Hat'] }
+
+    let!(:work) do
+      MusicalWork.new.tap do |work|
+        work.cover = cover
+        work.composer = ["Joe Schmo"]
+        work.title = ["My Special Song"]
+        work.genre = ["Pop"]
+        work.composition_date = ["today"]
+        work.apply_depositor_metadata("user")
+        work.visibility = "open"
+        work.save
+      end
+    end
+
+    it 'facets by cover subject' do
+      visit(root_path)
+
+      fill_in(:q, with: work.title.first)
+      click_button("Go")
+      save_and_open_page
+      
+      within('.facets') do
+        expect(page).to have_selector 'div.blacklight-cover_sim', text: 'Cover Subject'
+        expect(page).to have_content(work.cover.first)
+      end
+    end
+  end
 end
