8b70c92249b33e6e32e4d02f73478fb560badc38
Tom Johnson <tom@dp.la>
2017-05-07 14:59:28 -0700

    Add test for cover field on musical work

--- spec/models/musical_work_spec.rb
+++ spec/models/musical_work_spec.rb
@@ -2,8 +2,9 @@
 #  `rails generate hyrax:work MusicalWork`
 require 'rails_helper'
 
-RSpec.describe MusicalWork do
+RSpec.describe MusicalWork, type: :unit do
   it { is_expected.to respond_to(:composer) }
   it { is_expected.to respond_to(:genre) }
   it { is_expected.to respond_to(:composition_date) }
+  it { is_expected.to respond_to(:cover) }
 end
