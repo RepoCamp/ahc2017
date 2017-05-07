47f74af71dc407fcd39a6c9fb25183f92edc3b28
Tom Johnson <tom@dp.la>
2017-05-07 15:17:18 -0700

    Make cover subject visible in presenters

--- app/presenters/musical_work_presenter.rb
+++ app/presenters/musical_work_presenter.rb
@@ -1,4 +1,7 @@
 class MusicalWorkPresenter < Hyrax::WorkShowPresenter
   delegate :title, :composer, :genre, :composition_date, to: :solr_document
 
+  def cover_subject
+    solr_document.cover
+  end
 end
