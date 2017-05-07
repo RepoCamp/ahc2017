551982240ecb3b060e109dff478d266f8fb6fafd
Tom Johnson <tom@dp.la>
2017-05-07 15:16:30 -0700

    Add feature and unit tests for cover presentation

--- spec/presenters/musical_work_presenter_spec.rb
+++ spec/presenters/musical_work_presenter_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe MusicalWorkPresenter do
+describe MusicalWorkPresenter, type: :unit do
   let(:work) do
     MusicalWork.new.tap do |work|
       work.id = "work-id"
@@ -8,6 +8,7 @@ describe MusicalWorkPresenter do
       work.title = ["Song"]
       work.genre =  ["Pop"]
       work.composition_date = ["today"]
+      work.cover = ["Magic Hat"]
     end
   end
 
@@ -21,4 +22,5 @@ describe MusicalWorkPresenter do
   its(:composer) { is_expected.to eq(["Joe Schmo"]) }
   its(:genre) { is_expected.to eq(["Pop"]) }
   its(:composition_date) { is_expected.to eq(["today"]) }
+  its(:cover_subject)    { is_expected.to contain_exactly("Magic Hat") }
 end
